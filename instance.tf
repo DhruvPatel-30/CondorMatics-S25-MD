a9e5a83926ba3092d3341c982f0ab1a897ecd915
Dhruv <zeelpatel7573@gmail.com>
2025-06-11 19:06:19 -0400

    initial server setup

--- instance.tf
+++ instance.tf
@@ -1,23 +1,16 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
 # INSTANCES
 resource "aws_instance" "nginx1" {
   ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
   instance_type = "t3.micro"
 
+  iam_instance_profile = aws_iam_instance_profile.nginx_profile.name
+
   subnet_id       = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.public_security_group.id]
 
 
-  user_data = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
+  user_data = file("./initial-server-setup.sh")
 
 }
 
@@ -26,9 +19,16 @@ resource "aws_instance" "nginx2" {
   ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
   instance_type = "t3.micro"
 
+  iam_instance_profile = aws_iam_instance_profile.nginx_profile.name
+
   subnet_id       = aws_subnet.public_subnet_2.id
   security_groups = [aws_security_group.public_security_group.id]
 
   user_data = file("./initial-server-setup.sh")
 
+}
+
+resource "aws_iam_instance_profile" "nginx_profile" {
+  name = "nginx-profile"
+  role = "LabRole"
 }
